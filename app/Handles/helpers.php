5b9fe607c724a6cd4a1a1b8b747c74903992cf06
张俊加 <1020446694@qq.com>
2018-06-02 12:53:34 +0800

    构建基础首页页面

--- app/Handles/helpers.php
+++ app/Handles/helpers.php
@@ -5,3 +5,13 @@
  * Date: 2018/5/30
  * Time: 下午2:11
  */
+if ( !function_exists('route_class') ) {
+    /**
+     * 将当前的路由用-连接起来当做类名
+     * @return string
+     */
+    function route_class()
+    {
+        return str_replace('.', '-',request()->route()->getName());
+    }
+}
