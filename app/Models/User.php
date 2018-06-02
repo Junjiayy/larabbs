0719e563efa3d1b11be626cc7731bdc9196d61d4
张俊加 <1020446694@qq.com>
2018-06-02 21:46:20 +0800

    用户个人页面显示话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','introduction','avatar'
+        'name', 'email', 'password', 'introduction', 'avatar'
     ];
 
     /**
@@ -29,7 +29,13 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token',];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function topics ()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
