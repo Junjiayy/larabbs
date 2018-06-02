51b4acfb0e1b5fe7454d3c63b668df48f1a37ede
张俊加 <1020446694@qq.com>
2018-06-02 17:22:53 +0800

    用户上传头像功能

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 class RedirectIfAuthenticated
 {
     /**
-     * Handles an incoming request.
+     * Handlers an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
