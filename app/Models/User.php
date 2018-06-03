6836f59355fc7a7e28358f4c859bbee5fa348835
张俊加 <1020446694@qq.com>
2018-06-03 21:33:32 +0800

    侧边栏活跃用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\ActiveUserHelper;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -16,7 +17,7 @@ use Spatie\Permission\Traits\HasRoles;
  */
 class User extends Authenticatable
 {
-    use HasRoles,Notifiable {
+    use ActiveUserHelper,HasRoles,Notifiable {
         notify as protected laravelNotify;
     }
 
